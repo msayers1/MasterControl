c8b5ca848d4b58a06ee5514c3f38c792b8e09b7d
Michael Sayers <msayers1@gmail.com>
2024-09-14 10:11:17 -0400

    graphic display

--- map.py
+++ map.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 from settings import *
 _ = False
 # mini_map = [
@@ -31,5 +32,17 @@ class Map:
                 if value:
                     self.world_map[(i,j)] = value
     def draw(self):
-        [pg.draw.rect(self.display.screen, 'darkgray', (pos[0] * 10, pos[1] * 10, 10, 10), 2)
-            for pos in self.world_map]
+        #[pg.draw.rect(self.display.screen, 'darkgray', (pos[0] * 10, pos[1] * 10, 10, 10), 2)
+        #    for pos in self.world_map]
+        for pos, value in self.world_map.items():
+            if(value == -1):
+                color = 'darkgray'
+                pg.draw.rect(self.display.screen, color, (pos[0] * 10, pos[1] * 10, 10, 10))
+            else:
+                # Generate a random integer between 0 and 16777215 (0xFFFFFF in hexadecimal)
+                random.seed(value)
+                random_number = random.randint(0, 0xFFFFFF)
+                # Convert the number to hexadecimal and format it as a color string
+                color = "#{:06X}".format(random_number)
+                pg.draw.circle(self.display.screen, color, (pos[0] * 10, pos[1] * 10), 10)
+                # color = '#00FF00'
